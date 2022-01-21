b797210c0520ad33e2fb7f445e034d41476e7711
Dariusz <dariusz.borek@gmail.com>
2022-01-21 01:42:43 +0100

    first commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,21 @@
+package com.kodilla;
+
+class Calculator {
+
+    public void addAToB (int intA, int intB) {
+        int result = intA + intB;
+        System.out.println(result);
+    }
+
+    public void subtractAFromB (int intA, int intB) {
+        int result = intA - intB;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        calculator.addAToB(2, 2);
+        calculator.subtractAFromB(10, 5);
+    }
+}
