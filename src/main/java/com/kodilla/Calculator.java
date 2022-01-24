8ab23ee406e437b4a3e2df23cbdd2ed2c9d89eb4
Dariusz <dariusz.borek@gmail.com>
2022-01-24 22:16:56 +0100

    remove spring

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -2,12 +2,12 @@ package com.kodilla;
 
 class Calculator {
 
-    public void addAToB (int intA, int intB) {
+    public void addAToB(int intA, int intB) {
         int result = intA + intB;
         System.out.println(result);
     }
 
-    public void subtractAFromB (int intA, int intB) {
+    public void subtractAFromB(int intA, int intB) {
         int result = intA - intB;
         System.out.println(result);
     }
